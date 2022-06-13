7ec1590afe75374159087301ae0dfb40ea01466b
jhonatacaiob <jhonatacaiob@gmail.com>
2022-06-12 23:01:29 -0300

    feat(database): Adiciona migrations de criação das entidades Reservations e Borrowed

---
+++ priv/repo/migrations/20220613000835_create_reservations.exs
@@ -0,0 +1,29 @@
+defmodule OpenLibraryappApi.Repo.Migrations.CreateReservations do
+  use Ecto.Migration
+
+  def change do
+    create table(:reservations, primary_key: false) do
+
+      add :uuid, :uuid, primary_key: true
+      add :title, :varchar
+      add :pickup_date, :utc_datetime
+
+      add :book_id, references(
+        "books",
+        column: :uuid,
+        type: :uuid,
+        on_update: :nothing,
+        on_delete: :nothing,
+      )
+      add :user_id, references(
+        "users",
+        column: :uuid,
+        type: :uuid,
+        on_update: :nothing,
+        on_delete: :nothing,
+      )
+
+      timestamps()
+    end
+  end
+end
---
+++ priv/repo/migrations/20220613000846_create_borrowed.exs
@@ -0,0 +1,36 @@
+defmodule OpenLibraryappApi.Repo.Migrations.CreateBorrowed do
+  use Ecto.Migration
+
+  def change do
+    create table(:borrowed, primary_key: false) do
+
+      add :uuid, :uuid, primary_key: true
+      add :additional_note, :text
+      add :devolution_date, :utc_datetime
+
+      add :book_id, references(
+        "books",
+        column: :uuid,
+        type: :uuid,
+        on_update: :nothing,
+        on_delete: :nothing,
+      )
+      add :user_id, references(
+        "users",
+        column: :uuid,
+        type: :uuid,
+        on_update: :nothing,
+        on_delete: :nothing,
+      )
+      add :librarian_id, references(
+        "users",
+        column: :uuid,
+        type: :uuid,
+        on_update: :nothing,
+        on_delete: :nothing,
+      )
+
+      timestamps()
+    end
+  end
+end
